8068f585a6a5f484889b75a5f312741640c594e8
ih4t3youall <ih4t3youall@gmail.com>
2019-05-16 17:51:41 -0300

    minor fixes

--- youtube/index.js
+++ youtube/index.js
@@ -7,51 +7,77 @@ var app     = express();
 var videoUrls = new Set();
 var defaultUrl = "https://www.youtube.com";
 var myVariable ="";
+var songs =[];
 
 app.get('/scrape', function(req, res){
 
-	//url = 'http://www.imdb.com/title/tt1229340/';
-	//url = 'http://www.fabio.com.ar';
-	url = 'https://www.youtube.com/results?search_query=polkageist';
+	readFile();
 
-	//	yt-simple-endpoint style-scope ytd-video-renderer
-
-	request(url, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
-			//$('.vve-check').filter(function(index,item){
-			//yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink      spf-link
-			$('.yt-lockup.yt-lockup-tile.yt-lockup-video.vve-check.clearfix').each(function(index,item){
-				var searchPage =$(this).html();
-				$ = cheerio.load(searchPage);
-				$('a').each(function (index,item){
-					var text = $(this).attr('href')+'\n';
-					if (text.indexOf('watch') > -1){
-						videoUrls.add(text);
-					}
+	songs.forEach(function(item){
+		url = 'https://www.youtube.com/results?search_query='+item;
+		request(url, function(error, response, html){
+			if(!error){
+				var $ = cheerio.load(html);
+				$('.yt-lockup.yt-lockup-tile.yt-lockup-video.vve-check.clearfix').each(function(index,item){
+					var searchPage =$(this).html();
+					$ = cheerio.load(searchPage);
+					$('a').each(function (index,item){
+						var text = $(this).attr('href')+'\n';
+						console.log(text);
+						if (text.indexOf('watch') > -1){
+							videoUrls.add(defaultUrl+text);
+						}
+					});
 				});
-			});
-			saveToFile();
-		}
+			}
+		})
 	})
+	saveToFile();
 })
 
+function readFile(){
+	var lineReader = require('readline').createInterface({
+		input: require('fs').createReadStream('songs.txt')
+	});
+
+	lineReader.on('line', function (line) {
+		songs.push(line);
+	});
+}
+
+app.get('/results', function(req, res){
+	var vara ="";
+	for (var videos of videoUrls){
+		vara+=defaultUrl+videos+"\n";
+	}
+
+	fs.writeFile('final.txt',vara,(err)=>{
+		if(err)
+			console.log("error");
+
+	});
+
+
+})
 
 function saveToFile(){
+	console.log("savin to file");
 	for (var se of videoUrls ){
 		myVariable+= se;	
 	}
+	console.log("my variable content");
+	myVariable.forEach(x -> console.log(x));
+	console.log(".......................");
+
 	fs.unlink('othermessage.txt', (err) => {
 		if (err) throw err;
-		console.log('successfully deleted /tmp/hello');
+		console.log('successfully deleted otherText');
 
 		fs.writeFile('othermessage.txt',myVariable,(err)=>{
 			if(err){
 				console.log("error");
 			}else{
 				console.log("written");
-				videoUrls.clear();
-				myVariable = "";
 			}
 
 		});
